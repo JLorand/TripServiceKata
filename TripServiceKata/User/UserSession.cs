9b7cec5933a7e7dffba8833d95383c353c7e3074
lorand.jakab <jakablorand@gmail.com>
2023-06-11 20:36:28 +0200

    Initial commit

---
+++ TripServiceKata/User/UserSession.cs
@@ -0,0 +1,28 @@
+﻿using TripServiceKata.Exception;
+
+namespace TripServiceKata.User
+{
+    public class UserSession
+    {
+        private static readonly UserSession Session = new();
+
+        private UserSession() { }
+
+        public static UserSession GetInstance()
+        {
+            return Session;
+        }
+
+        public static bool IsUserLoggedIn(User user)
+        {
+            throw new DependentClassCallDuringUnitTestException(
+                "UserSession.IsUserLoggedIn() should not be called in an unit test");
+        }
+
+        public User? GetLoggedUser()
+        {
+            throw new DependentClassCallDuringUnitTestException(
+                "UserSession.GetLoggedUser() should not be called in an unit test");
+        }
+    }
+}
---
+++ TripsServiceKata.Tests/TripServiceTests.cs
@@ -0,0 +1,8 @@
+using TripServiceKata.Trip;
+using TripServiceKata.User;
+
+namespace TripsServiceKata.Tests;
+
+public class TripServiceTests
+{
+}
